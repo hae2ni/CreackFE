e9a2fdd18fbca2b2be143c6a6723b9008d0b82f4
HAE2NI <haein990222@naver.com>
2023-02-11 22:35:12 +0900

    🎉 start

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native/types';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>project</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
