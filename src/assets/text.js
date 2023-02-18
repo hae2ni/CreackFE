e109417796a66fdd6489f5d75d975cdc25d947c9
HAE2NI <haein990222@naver.com>
2023-02-18 22:49:15 +0900

    💄 default text style

---
+++ src/assets/text.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+export const display2 = styled.text`
+  font-family: 'Pretendard';
+  font-style: 'normal';
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  color: #1c1c1c;
+`;
+
+export const caption1 = styled.text`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #606060;
+`;
+
+export const display4 = styled.text`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: #1c1c1c;
+`;
